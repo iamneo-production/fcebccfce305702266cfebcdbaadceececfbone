c4d2d7c450d15f7442cc09154fc0c9b64a05d934
Iamneo <techadmin@iamneo.ai>
2023-10-03 05:25:33 +0000

    Backup Commit

--- angularapp/src/app/app.component.spec.ts
+++ angularapp/src/app/app.component.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-// import { BusBookingFormComponent } from './bus-booking-form/bus-booking-form.component';
+import { BusBookingFormComponent } from './bus-booking-form/bus-booking-form.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    declarations: [AppComponent, BusBookingFormComponent]
   }));
 
   it('should have as title Bus Booking App', () => {
